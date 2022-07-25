429e29940a3f2ce121f00ee9b98fb2eb85fb023a
Mark Levine <mark-levine-1@outlook.com>
2022-07-24 21:04:08 -0700

    trying to reset the database again to use dj database url config

--- newsagg/settings.py
+++ newsagg/settings.py
@@ -83,18 +83,34 @@ WSGI_APPLICATION = 'newsagg.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+import dj_database_url
+
+# DATABASES['default'].update(dj_database_url.config(conn_max_age=600))
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(),
 }
 
 
-import dj_database_url
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'test',
+#         'USER': 'postgres',
+#         'PASSWORD': 'test1234',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
-DATABASES['default'].update(dj_database_url.config(conn_max_age=600))
+
 
 
 
