cedf855f964d39d5ee07fc487d4c0a11c153c4a0
Mark Levine <mark-levine-1@outlook.com>
2022-07-24 20:34:25 -0700

    blaaaa

--- newsagg/settings.py
+++ newsagg/settings.py
@@ -91,10 +91,10 @@ DATABASES = {
     }
 }
 
-import dj_database_url
+# import dj_database_url
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'] = dj_database_url.config(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'] = dj_database_url.config(db_from_env)
 
 
 
