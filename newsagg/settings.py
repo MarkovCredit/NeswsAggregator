1a558d37a4a3e23fa2c8cc415cbb8c3433f1390b
Mark Levine <mark-levine-1@outlook.com>
2022-07-24 18:32:21 -0700

    chaning config

--- newsagg/settings.py
+++ newsagg/settings.py
@@ -130,7 +130,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # print(f'Base Dir is {BASE_DIR}')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
